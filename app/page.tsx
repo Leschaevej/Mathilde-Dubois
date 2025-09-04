e82c9ee1882a18ef30eeca6bf4ebf764bb403686
leschaeve.jimmy <leschaeve.jimmy@gmail.com>
2025-09-04 13:42:06 +0200

    feat: hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
 import "./page.scss";
 
 export default function Home() {
-
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <h2>Mathilde Dubois</h2>
+                <p>Dessinatrice<br/>en bâtiment</p>
+            </div>
+        </section>
+    )
 }
